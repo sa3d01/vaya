389907e55fe7aaa977ae26a90d17c61dd3fb7b7c
sa3d01 <sa3dsalem01@gmail.com>
2022-01-11 22:11:36 +0200

    contact us chat ,about

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Admin\Entities\Admin;
 
 class Chat extends Model
 {
@@ -16,4 +17,20 @@ class Chat extends Model
         'message',
         'read',
     ];
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class);
+    }
+    public function brand()
+    {
+        return $this->belongsTo(BrandOwner::class,'brand_id','id');
+    }
 }
