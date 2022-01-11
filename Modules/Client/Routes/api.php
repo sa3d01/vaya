7ee8daf9df9a19ddf3ed8ecdbd3854026d1cf14a
sa3d01 <sa3dsalem01@gmail.com>
2022-01-11 20:13:12 +0200

    check client token

--- Modules/Client/Routes/api.php
+++ Modules/Client/Routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Brand\Http\Middleware\CheckApiToken;
+use Modules\Client\Http\Middleware\CheckApiToken;
 
 /*
 |--------------------------------------------------------------------------
