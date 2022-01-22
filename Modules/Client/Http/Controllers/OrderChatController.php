407b1a7addd8fab0d6e7b65625a9e7506898b6a9
sa3d01 <sa3dsalem01@gmail.com>
2022-01-22 17:34:16 +0200

    chat order

--- Modules/Client/Http/Controllers/OrderChatController.php
+++ Modules/Client/Http/Controllers/OrderChatController.php
@@ -2,7 +2,10 @@
 
 namespace Modules\Client\Http\Controllers;
 
+use App\Models\Brand;
 use App\Models\Chat;
+use App\Models\Order;
+use Illuminate\Http\Request;
 use Modules\Client\Transformers\ChatOrderDTO;
 
 class OrderChatController extends MasterController
@@ -10,6 +13,16 @@ class OrderChatController extends MasterController
     public function messages($order_id)
     {
         $chats = Chat::where('order_id',$order_id)->get();
+        return $this->sendResponse(ChatOrderDTO::collection($chats));
+    }
+    public function store(Request $request)
+    {
+        $input = $request->only('message','order_id');
+        $order=Order::find($request['order_id']);
+        $input['client_id'] = auth('client')->id();
+        $input['brand_id'] = $order->brand_id;
+        Chat::create($input);
+        $chats = Chat::where('order_id',$request['order_id'])->get();
         return $this->sendResponse(ChatOrderDTO::collection($chats));
     }
 }
